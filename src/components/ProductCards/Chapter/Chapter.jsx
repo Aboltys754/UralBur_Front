9d82a33f36db135b258f1fdf26fa5f5e4d457d4c
Aboltys754 <warwar754@mail.ru>
2025-01-10 15:01:08 +0500

    немного разобрался с карточками

---
+++ src/components/ProductCards/Chapter/Chapter.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Chapter() {
+    return <div>Hello World</div>
+}
--- src/components/ProductCards/ProductCards.jsx
+++ src/components/ProductCards/ProductCards.jsx
@@ -1,28 +1,46 @@
 import serviceHost from "../libs/service.host.js";
 import connector from "../libs/connector.js";
-import styles from "./styles.module.css"
+import Chapter from './Chapter/Chapter.js';
 
 
 connector.add("ProductCards");
+const objectAllChapter = {};
 
-function ProductCards({ levels, positions }) {
+function ProductCards({ levels }) {
     React.useEffect(() => connector.del("ProductCards"));
-    const [lev, useLev] = React.useState(0);
-    levels.map((level) => console.log(level))
-    positions.map((position) => console.log(position));
-    if (lev === 0) {
-        return (
-            <div className={styles.root}>
-                {levels.map((level) => <button key={level.id}>
-                <img src={`${serviceHost("mcontent")}/api/mcontent/static/images/catalog/${level.image.fileName}`}></img>
-                <p>{level.title}</p>
-             </button>)
-             }
-            </div>
-        )
-    }
-  }
+    const [lev, setLev] = React.useState('0');
+    const [levValue, setLevValue] = React.useState([]);    
+    
+    levels.map((lev, index) => _parseLevels(lev, '0' + index));
+    const mapppp = _createArreyList(lev, objectAllChapter)
+    return (
+        <>  
+        {/* <button onClick={() => setLev(lev + '0')}>+</button>
+        <button>-</button> */}
+            {mapppp.map((level) =>                 
+                <button key={level.id} className="producstCardsButton">
+                    <p className="producstCardsButtonP">{level.title}</p>
+                    <img className="producstCardsButtonImg" src={`${serviceHost("mcontent")}/api/mcontent/static/images/catalog/${level.image.fileName}`}></img>                    
+                </button>)}
+        </>
+    )};
 
+
+function _parseLevels(firstValue, firstKey) {
+    if ((firstValue.childs).length === 0) {
+        objectAllChapter[firstKey] = firstValue;
+    } else {
+        objectAllChapter[firstKey] = firstValue;
+        (firstValue.childs).map((nextValue, nextKey) => _parseLevels(nextValue, firstKey.toString() + nextKey.toString()))}
+    };
+function _createArreyList(lev, maps) {
+    const temp_maps = []
+    for (let key in maps) {
+        if (key.slice(0, -1) === lev) {
+            temp_maps.push(maps[key]);
+        }};
+    return temp_maps
+}
  
 const levelsFetch = fetch(`${serviceHost("mcontent")}/api/mcontent/catalog/level/public`);
 const positionsFetch = fetch(`${serviceHost("mcontent")}/api/mcontent/catalog/position/public`);
@@ -31,5 +49,6 @@ Promise.all([levelsFetch, positionsFetch])
     .then(responses => Promise.all(responses.map(async res => await res.json())))
     .then(responses => {
     const root = ReactDOM.createRoot(document.getElementById("productCards"));
-    root.render(<ProductCards levels={responses[0]} positions={responses[1]}/>);
+    root.render(<ProductCards levels={responses[0]} />);
     });
+
